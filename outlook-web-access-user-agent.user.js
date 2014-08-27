7aed1ed4154c1b1fd0fde3e73e5a4f3ead023778
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-08-27 11:12:46 -0300

    Adding OWA user agent

---
+++ outlook-web-access-user-agent.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       Change User-Agent for OWA
+// @namespace  http://gmatheu.github.io/
+// @version    0.1
+// @description Uses Mozilla user agent, so OWA rich client can be displayed
+// @match      https://*/owa/*
+// @copyright  2014, Gonzalo Matheu 
+// @user-agent Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0
+// ==/UserScript==
