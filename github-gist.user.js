2c4a3a6fa9b6f8c67d347fc7ffa4bb6575911859
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-10-20 14:41:12 -0200

    Adding gist

---
+++ github-gist.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       Github Gist
+// @namespace  http://gmatheu.github.io/
+// @version    0.1
+// @description  More space for gists
+// @match      https://gist.github.com/*/*
+// @copyright  2014+, Gonzalo Matheu
+// ==/UserScript==
+
+
+///////////////////////////////////////////////////////////////
+// Style
+///////////////////////////////////////////////////////////////
+$(".container").width("90%");
+$(".column").width("85%");
